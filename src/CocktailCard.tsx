eb67be6562c22fd3515e5f7943813380860cfe7c
aronsantha <santhaaron@gmail.com>
2024-03-02 21:51:15 +0100

    add tailwind

---
+++ src/CocktailCard.tsx
@@ -0,0 +1,11 @@
+function CocktailCard() {
+  const greeting = "hello";
+
+  return (
+    <>
+      <div className="!bg-red-200 font-semibold"> {greeting} world</div>
+    </>
+  );
+}
+
+export default CocktailCard;
