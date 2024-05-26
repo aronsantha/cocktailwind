74e0314336cb7f544d3697142235fb0b64d2a026
aronsantha <santhaaron@gmail.com>
2024-05-26 16:22:47 +0200

    add tenstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { CocktailCard } from "./CocktailCard";
 import { XCircleIcon } from "@heroicons/react/24/solid";
-
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 export interface Cocktail {
@@ -57,25 +57,37 @@ export interface Cocktail {
 }
 
 export const App = () => {
-  const [cocktails, setCocktails] = useState<Cocktail[]>();
   const [selectedCocktailId, setSelectedCocktailId] = useState<number | null>();
-  const [selectedCocktail, setSelectedCocktail] = useState<Cocktail | null>(
-    null,
-  );
   const [ingredients, setIngredients] = useState<string[]>([]);
 
-  const handleDataFromChild = () => {
-    if (!selectedCocktailId) {
-      return;
-    }
-    fetchCocktail(selectedCocktailId).then((response) =>
-      setSelectedCocktail(response),
-    );
-  };
+  const { data: cocktails } = useQuery({
+    queryKey: ["cocktails"],
+    queryFn: () =>
+      fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail`,
+      ).then((response) =>
+        response.json().then((data) => (data.drinks as Cocktail[]) || []),
+      ),
+  });
 
-  useEffect(() => {
-    handleDataFromChild();
-  }, [selectedCocktailId]);
+  const cocktailBuildingMethods: string[] = [
+    "Building",
+    "Shaking",
+    "Stirring",
+    "Garnishing",
+    "Pouring",
+  ];
+
+  const { data: selectedCocktail, isLoading: isLoadingCocktail } = useQuery({
+    queryKey: ["selectedCocktail", selectedCocktailId],
+    queryFn: () =>
+      fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${selectedCocktailId}`,
+      ).then((response) =>
+        response.json().then((data) => data.drinks[0] as Cocktail),
+      ),
+    enabled: !!selectedCocktailId,
+  });
 
   useEffect(() => {
     if (selectedCocktail) {
@@ -91,76 +103,63 @@ export const App = () => {
   }, [selectedCocktail]);
 
   const closeModal = () => {
-    setSelectedCocktail(null);
     setSelectedCocktailId(null);
   };
 
-  const fetchCocktails = async () => {
-    const response = await fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail`,
-    );
-    const fetchedCocktails = await response.json();
-    setCocktails(fetchedCocktails.drinks);
-  };
-
-  const fetchCocktail = async (cocktailId: number) => {
-    const response = await fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`,
-    );
-    const fetchedCocktail = await response.json();
-    return fetchedCocktail.drinks[0] as Cocktail;
-  };
-
-  useEffect(() => {
-    fetchCocktails();
-  }, []);
-
   return (
-    <div className="mx-auto flex w-screen max-w-screen-2xl flex-col items-center  p-20">
-      {selectedCocktail && (
+    <div className="mx-auto flex w-screen max-w-screen-2xl flex-col items-center p-8 md:p-14">
+      {selectedCocktailId && (
         <section>
           <div
             onClick={closeModal}
             className="fixed inset-0 z-50 cursor-pointer bg-black/50 backdrop-blur-sm"
           ></div>
-
           <div className="pointer-events-none fixed inset-0 z-[999] flex items-center justify-center">
-            <div className="pointer-events-auto mx-12 flex w-[800px] max-w-full flex-col overflow-clip rounded-lg bg-white">
-              <div className="flex flex-row">
-                <div
-                  className="relative left-0 top-0  bg-gray-400
+            <div className="pointer-events-auto mx-12 flex min-h-[200px] w-[800px] max-w-full flex-col overflow-clip rounded-lg bg-white">
+              {isLoadingCocktail ? (
+                <div className="mx-auto my-auto animate-pulse">
+                  {!cocktailBuildingMethods.length
+                    ? "Loading"
+                    : cocktailBuildingMethods[Math.floor(Math.random() * 5)]}
+                  {" your cocktail..."}
+                </div>
+              ) : !selectedCocktail ? null : (
+                <div className="flex flex-row">
+                  <div
+                    className="relative left-0 top-0  bg-gray-400
                  "
-                >
-                  <img
-                    className="min-h-[100%] min-w-[100%]"
-                    src={selectedCocktail.strDrinkThumb}
-                    alt={selectedCocktail.strDrink}
-                  />
-                </div>
-                <div className="relative flex w-full flex-col gap-4 p-12 ">
-                  <button
-                    onClick={closeModal}
-                    className="absolute right-5 top-5"
                   >
-                    <XCircleIcon className="h-[35px] text-black opacity-20 hover:opacity-100" />
-                  </button>
-                  <h1 className="font-pacifico text-2xl font-bold">
-                    {selectedCocktail.strDrink}
-                  </h1>
-                  <div>
-                    <div className="mb-2 font-medium">Glass</div>
-                    <div>{selectedCocktail.strGlass}</div>
+                    <img
+                      className="min-h-[100%] min-w-[100%]"
+                      src={selectedCocktail.strDrinkThumb}
+                      alt={selectedCocktail.strDrink}
+                    />
                   </div>
-                  <div>
-                    <div className="mb-2 font-medium">Ingredients</div>
-                    <ul className="list-disc pl-[20px]">
-                      {ingredients?.map((ingredient, index) => (
-                        <li key={index}> {ingredient}</li>
-                      ))}
-                    </ul>
+                  <div className="relative flex w-full flex-col gap-4 p-12 ">
+                    <button
+                      onClick={closeModal}
+                      className="absolute right-5 top-5"
+                    >
+                      <XCircleIcon className="h-[35px] text-black opacity-20 hover:opacity-100" />
+                    </button>
+                    <h1 className="font-pacifico text-2xl font-bold">
+                      {selectedCocktail.strDrink}
+                    </h1>
+                    <div>
+                      <div className="mb-2 font-medium">Glass</div>
+                      <div>{selectedCocktail.strGlass}</div>
+                    </div>
+                    <div>
+                      <div className="mb-2 font-medium">Ingredients</div>
+                      <ul className="list-disc pl-[20px]">
+                        {ingredients?.map((ingredient, index) => (
+                          <li key={index}> {ingredient}</li>
+                        ))}
+                      </ul>
+                    </div>
                   </div>
                 </div>
-              </div>
+              )}
             </div>
           </div>
         </section>
